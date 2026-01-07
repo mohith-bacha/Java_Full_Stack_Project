53246b72394acbecea20512babfa2cf710f82974
Mohith Bacha <mohithbacha1@gmail.com>
2026-01-07 11:56:31 +0530

    Update LandingPage.js
    Landing page Dashboard name changes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -70,7 +70,7 @@ const LandingPage = () => {
               },
             }}
           >
-            Go to Dashboard
+            Go to CMRCET
           </Button>
         </Box>
 
@@ -151,7 +151,7 @@ const LandingPage = () => {
                     },
                   }}
                 >
-                  Go to Dashboard
+                  GO TO CMRCET
                 </Button>
               </CardContent>
             </Card>
